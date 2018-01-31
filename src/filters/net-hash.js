95f0a1e57452dd3fcdc5c8e6592a4f56edcf1a01
MarsCrypto <mars@marscrypto.com>
2018-01-31 18:13:17 +0800

    update nethash

--- src/filters/net-hash.js
+++ src/filters/net-hash.js
@@ -4,7 +4,7 @@ import AppFilters from './filters.module';
  * @todo check the possibility of removing hard coded hashes
  */
 AppFilters.filter('nethash', () => nethash => {
-	if (nethash === 'a0c38f0dd302a6cf5ceef3d9dc5ccf0a12a34ab39c0cc42cf2d2602d8b44b0ad') {
+	if (nethash === '52fd9a2f0857ca94510fae33c5e71b34eb5f9fa45813c5bbf6ee796c3731dbd9') {
 		return 'Testnet';
 	} else if (nethash === '463aeac28885fa5be9efc4d095900f622e3d9efac8c9317b7f1e8fe804d5a039') {
 		return 'Mainnet';

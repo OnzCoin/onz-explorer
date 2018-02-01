834d37bd18deaf01f0185d6fd84a51c0d129594d
kankando <kan@onzcoin.com>
2018-02-01 12:59:29 +0800

    update hash for testnet

--- src/filters/net-hash.js
+++ src/filters/net-hash.js
@@ -4,7 +4,7 @@ import AppFilters from './filters.module';
  * @todo check the possibility of removing hard coded hashes
  */
 AppFilters.filter('nethash', () => nethash => {
-	if (nethash === '52fd9a2f0857ca94510fae33c5e71b34eb5f9fa45813c5bbf6ee796c3731dbd9') {
+	if (nethash === '5e4182dfd5e50072651e0cdb75020c449941da694880fcea5aa4badf45e9e0d2') {
 		return 'Testnet';
 	} else if (nethash === '463aeac28885fa5be9efc4d095900f622e3d9efac8c9317b7f1e8fe804d5a039') {
 		return 'Mainnet';

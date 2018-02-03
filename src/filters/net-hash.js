57d40ca83cb05ac1b50b2b805707bd718afd80c1
kanwong <kanwong@gmail.com>
2018-02-03 12:18:14 +0800

    update to latest testnet hash

--- src/filters/net-hash.js
+++ src/filters/net-hash.js
@@ -4,7 +4,7 @@ import AppFilters from './filters.module';
  * @todo check the possibility of removing hard coded hashes
  */
 AppFilters.filter('nethash', () => nethash => {
-	if (nethash === '5e4182dfd5e50072651e0cdb75020c449941da694880fcea5aa4badf45e9e0d2') {
+	if (nethash === '570f81c67882dbcbebf8b84205a78a42cd98561683f878dae8e8c7ea2f54033b') {
 		return 'Testnet';
 	} else if (nethash === '463aeac28885fa5be9efc4d095900f622e3d9efac8c9317b7f1e8fe804d5a039') {
 		return 'Mainnet';

de7035cc873b25ba8a4712e888daff3a638b7f97
kanwong <kanwong@gmail.com>
2018-02-04 16:33:30 +0800

    finalized version and push it to new version 1.4.2 for testnet

--- src/filters/net-hash.js
+++ src/filters/net-hash.js
@@ -4,7 +4,7 @@ import AppFilters from './filters.module';
  * @todo check the possibility of removing hard coded hashes
  */
 AppFilters.filter('nethash', () => nethash => {
-	if (nethash === '570f81c67882dbcbebf8b84205a78a42cd98561683f878dae8e8c7ea2f54033b') {
+	if (nethash === 'aa14b4d84260e00b6fc033c022a25965629ab0e8a4aafc77e64cad4cf0dc2e00') {
 		return 'Testnet';
 	} else if (nethash === '463aeac28885fa5be9efc4d095900f622e3d9efac8c9317b7f1e8fe804d5a039') {
 		return 'Mainnet';

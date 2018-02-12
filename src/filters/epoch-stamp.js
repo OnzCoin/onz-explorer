471c614c96385b7879e35c527aec1efc72125156
kankando <kan@onzcoin.com>
2018-02-12 12:03:58 +0800

    update for the mainnet

--- src/filters/epoch-stamp.js
+++ src/filters/epoch-stamp.js
@@ -1,5 +1,5 @@
 import AppFilters from './filters.module';
 
 AppFilters.filter('epochStamp', () => d => new Date(
-	(((Date.UTC(2018, 2, 11, 17, 0, 0, 0) / 1000) + d) * 1000),
+	(((Date.UTC(2018, 1, 11, 17, 0, 0, 0) / 1000) + d) * 1000),
 ));
